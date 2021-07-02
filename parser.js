379a1c8f1b7aa7d0c5008ddb8f88d597f62719e9
Shekhar Tyagi <notshekhar@gmail.com>
2021-07-02 21:09:33 +0530

    new

---
+++ parser.js
@@ -0,0 +1,74 @@
+const code = `
+<!fdfhgsdgsdf>
+<details>
+    <name>shekhar</name>
+    <collage>
+        <name>gbu</name>
+    </collage>
+</details>
+`
+function Token(type, value) {
+    this.type = type
+    this.value = value
+}
+
+function lexer(code) {
+    const tokens = new Array()
+    let codeLength = code.length
+    let word = ""
+    let counter = 0
+    while (true) {
+        if (counter == codeLength) break
+        let c = code[counter]
+        if (c == " " || c == "\n" || c == "\t") {
+            counter++
+            continue
+        }
+        if (c == "<") {
+            word = ""
+            counter++
+            if (code[counter] == "!") {
+                while (code[counter] != ">") counter++
+                counter++
+                continue
+            }
+            if (code[counter] == "/") {
+                counter++
+                while (code[counter] != ">") {
+                    c = code[counter]
+                    word += c
+                    counter++
+                }
+                tokens.push(new Token("end-tag", word))
+                counter++
+                continue
+            }
+            while (code[counter] != ">") {
+                c = code[counter]
+                word += c
+                counter++
+            }
+            tokens.push(new Token("tag", word))
+            counter++
+            continue
+        } else {
+            word = ""
+            while (code[counter] != "<") {
+                c = code[counter]
+                word += c
+                counter++
+            }
+            tokens.push(new Token("value", word))
+            continue
+        }
+        counter++
+    }
+    return tokens
+}
+
+
+
+function parser(tokens) {
+    
+}
+parser(lexer(code))
